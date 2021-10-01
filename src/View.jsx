6b1cdaa6fe9a834f6b84d4bd047f0c95fc05dd9c
Sanjay <68421874+Sanjay7yadav@users.noreply.github.com>
2021-10-01 09:17:03 +0530

    Add files via upload

---
+++ src/View.jsx
@@ -0,0 +1,51 @@
+import { PromiseProvider } from 'mongoose';
+import React,{useEffect, useState} from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';  
+import { update } from './models/noteModel';
+
+const View=() =>{
+
+    const [products,setProducts]= useState([{
+        ItemId:'',
+        Image:'',
+            Category:'',
+            product_name:'',
+            product_Brand_Name:'',
+            Price:'',
+            Stock:'',
+            Discription:''
+    }])
+
+    useEffect(()=>{
+        fetch("/view").then(res=>{
+            if(res.ok){
+                return res.json()
+            }
+        }).then(jsonRes => setProducts(jsonRes));
+    })
+function Edit(id)
+{
+    console.log(id);
+    props.history.push("/update"+id)
+    
+}
+    return<div className='container'>
+            <h1>Items</h1>
+            {products.map(pro =>
+            <tr key={pro.product_name}>
+                <td>{pro.Image}</td>
+                <td>{pro.Category}</td>
+                <td>{pro.product_name}</td>
+                <td>{pro.product_Brand_Name}</td>
+                <td>{pro.Price}</td>
+                <td>{pro.Stock}</td>
+                <td>{pro.Discription}</td>
+                <td><button onClick={()=>Edit(pro.product_name)} className="btn btn-success">Update</button></td>
+                <td><button  className="btn btn-danger">Delete</button></td>
+            </tr>)}
+            
+        </div>
+    
+
+}
+export default View;
